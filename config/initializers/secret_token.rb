0b0974e5d5e68f4bb55764c36d9a86197407fb4a
Alex MacCaw <maccman@gmail.com>
2011-05-20 19:29:40 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Airguide::Application.config.secret_token = 'cf55a0e6dafda8066570a51259507a664e32296bdaf719aeaaf797247241c328433bcad852c89b594a7ebb66ba61f74423b96da6c084d2579cdeb615a041169b'
